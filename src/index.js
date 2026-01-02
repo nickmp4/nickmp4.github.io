6446b15304cc4e8021055799369fe9f15113c960
nick!! <113379836+nickmp4@users.noreply.github.com>
2026-01-02 14:54:53 -0600

    added clocl + minor css tweaks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 const cursor = document.querySelector(".cursor")
+var span = document.getElementById('clock')
 
 document.addEventListener("mousemove", (event)=> {
 
     cursor.style.left = `${event.clientX }px`
     cursor.style.top = `${event.clientY}px`
 })
+
+function time() {
+    var d = new Date();
+    var m = d.getMinutes();
+    var h = d.getHours();
+
+    m = checkTime(m);
+    h = checkTime(h);
+
+    span.textContent = h + ":" + m;
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    } return i;
+}
+
+setInterval(time, 1000);
